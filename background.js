cc10291b2f389a663cf2bef173e55f891d96bcb5
andrewjamesmoore <andrew.moore@samdesk.io>
2025-06-29 13:04:50 -0600

    fix: add chrome session storage

--- background.js
+++ background.js
@@ -1,27 +1,40 @@
-let lastTabId = null;
-let currentTabId = null;
+const storage = chrome.storage.session;
 
-chrome.tabs.onActivated.addListener(({ tabId }) => {
-  if (currentTabId !== null && currentTabId !== tabId) {
-    lastTabId = currentTabId;
+chrome.tabs.onActivated.addListener(async ({ tabId }) => {
+  const { currentTabId } = await storage.get("currentTabId");
+
+  if (currentTabId) {
+    await storage.set({ lastTabId: currentTabId });
   }
-  currentTabId = tabId;
+  await storage.set({ currentTabId: tabId });
 });
 
 chrome.commands.onCommand.addListener(async (command) => {
-  if (command === "switch-to-last-tab" && lastTabId !== null) {
-    try {
-      await chrome.tabs.update(lastTabId, { active: true });
-    } catch (error) {
-      console.warn(
-        `Could not switch to tab ${lastTabId}. It may have been closed.`
-      );
+  if (command === "switch-to-last-tab") {
+    const { lastTabId } = await storage.get("lastTabId");
+
+    if (lastTabId) {
+      try {
+        await chrome.tabs.update(lastTabId, { active: true });
+      } catch (error) {
+        console.warn(
+          `Could not switch to tab ${lastTabId}. It may have been closed.`
+        );
+      }
     }
   }
 });
 
-chrome.tabs.onRemoved.addListener((tabId) => {
+chrome.tabs.onRemoved.addListener(async (tabId) => {
+  const { lastTabId, currentTabId } = await storage.get([
+    "lastTabId",
+    "currentTabId",
+  ]);
+
   if (tabId === lastTabId) {
-    lastTabId = null;
+    await storage.remove("lastTabId");
+  }
+  if (tabId === currentTabId) {
+    await storage.remove("currentTabId");
   }
 });
